59dcc9c4146b37207e7ca883b72732e801921dcf
beattycharles <113803621+beattycharles@users.noreply.github.com>
2022-12-18 17:10:46 -0500

    resctructed index.js made fuctons for members

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
+let employeeInfo = [];
 
 console.log('Get ready to build your team!');
 //prompt list for input
@@ -21,77 +22,103 @@ inquirer
     name: 'managerOfficeNumber',
     message: 'Whats Mangers office number?'
     },
-    {type: 'list',
+])
+
+.then((responce) => {
+    const managerAnswer = []
+    managerAnswer.push(responce)
+        console.log(managerAnswer)
+        addMember();
+});
+//function for  adding Member
+        function addMember(){
+            inquirer
+            .prompt([
+        {type: 'list',
     message: 'What typoe of member to add?',
     name: 'members',
-    choices: ['Engineer', 'Intern', 'Done']
-    },
-])
-//save anwsers for use in html.
-.then((responce) => {
-    const memberAnswer = []
-    memberAnswer.push(responce)
-        console.log(memberAnswer)
-
-        if (members === "Engineer"){
+    choices: ['Engineer', 'Intern', 'Done'],
+            }
+        ])
+    
+    .then((anwsers) =>{
+    if(anwsers.members === 'Engineer') {
+        engineerPrompts();
+    } else if (anwsers.members === 'Intern') {
+        internPrompt();
+    } else {
+        genHtml();
+        return
+    }
+        })
+    };
+    
+// add engineer
+    function engineerPrompts() {
             inquirer
             .prompt([
                 {
-                    tyype: 'input',
+                    type: 'input',
                     name: 'engName',
                     message: 'Engineers Name?'
                 },
                 {
-                    tyype: 'input',
+                    type: 'input',
                     name: 'engId',
                     message: 'Engineers Id number?'
                 },
                 {
-                    tyype: 'input',
+                    type: 'input',
                     name: 'engEmail',
                     message: 'Engineers Email?'
                 },
                 {
-                    tyype: 'input',
+                    type: 'input',
                     name: 'engGithub',
                     message: 'Engineers Github?'
                 },
             ])
-            .then(callback(members))
-            memberAnswer.push(answers)
-            console.log(memberAnswers)
-        }
-         else if (members === 'Intern') {
-            inquirer
+            .then((anwsers) => {
+                let engInfo = new Engineer(anwsers.engName, anwsers.engId, anwsers.engEmail, anwsers.engGithub)
+                employeeInfo.push(engInfo)
+                addMember();
+            }
+            )};
+
+//add intern
+        function internPrompt() {
+        inquirer
             .prompt([
-                {
-                    type: 'input',
-                    name: 'interName',
-                    message: 'What is the interns name?'
-                },
-                {
-                    type: 'input',
-                    name: 'interId',
-                    message: 'What is the interns Id number?'
-                },
-                {
-                    type: 'input',
-                    name: 'interEmail',
-                    message: 'What is the interns email?'
-                },
-                {
-                    type: 'input',
-                    name: 'interSchool',
-                    message: 'What school does the intern attend?'
-                },
+            {
+            type: "input",
+            name: "internName",
+            message: "What is the interns name?",
+            },
+            {
+            type: "input",
+            name: "internId",
+            message: "What is the interns Id number?",
+            },
+            {
+            type: "input",
+            name: "internEmail",
+            message: "What is the interns email?",
+            },
+            {
+            type: "input",
+            name: "internSchool",
+            message: "What school does the intern attend?",
+            },
             ])
-            .then(callback(members))
-            memberAnswer.push(answers)
-            console.log(memberAnswer)
-         }
-         else {
-            fs.writeFile('./dist/team.html', memberAnswer, (err) =>
-            err ? console.log(err) : console.log('Profiles have been made for your team!')
-            ) 
-        }
-    });
+            .then((anwsers) => {
+                let internInfo = new Inter(anwsers.internName, anwsers.internId, anwsers.internEmail, anwsers.internSchool)
+                employeeInfo.push(internInfo)
+                addMember();
+            })};
+        
+    // when done adding members make hmtl file
+        function genHtml(){
+            const html = gererateHtml(employeeInfo);
+           fs.writeFile('./dist/team.html', html, (err) =>
+           err ? console.log(err) : console.log('Profiles have been made for your team!')
+           )};
--- test/Employee.test.js
+++ test/Employee.test.js
@@ -1,29 +1,29 @@
-const Employee = require('../lib/employee');
+const Employee = require("../lib/employee");
 
 describe("employee", () =>
-describe('Get', () => {
-    it('Get the name of the employee', () => {
-        const Employee = ('charlie', 1, 'no@no.com')
-        const result = Employee.name;
-        expect(result).toString('charlie')
+  describe("Get", () => {
+    it("Get the name of the employee", () => {
+      const Employee = ("charlie", 1, "no@no.com");
+      const result = Employee.name;
+      expect(result).toString("charlie");
     });
 
-    it('Get id of employee', () => {
-        const Employee = ('charlie', '1', 'no@no.com')
-        const result = Employee.getId;
-        expect(result).toString('1')
+    it("Get id of employee", () => {
+      const Employee = ("charlie", 1, "no@no.com");
+      const result = Employee.getId;
+      expect(result).toString(1);
     });
 
-    it('Get email of employee', () => {
-        const Employee = ('charlie', 1, 'no@no.com')
-        const result = Employee.email;
-        expect(result).toString('no@no.com');
+    it("Get email of employee", () => {
+      const Employee = ("charlie", 1, "no@no.com");
+      const result = Employee.email;
+      expect(result).toString("no@no.com");
     });
 
-    it('Get role of the employee', () => {
-        const employee = ('charlie', 1, 'no@no.com')
-        employee.role = 'Employee';
-        const result = Employee.role;
-        expect(result).toString('Employee');
+    it("Get role of the employee", () => {
+      const employee = ("charlie", 1, "no@no.com");
+      employee.role = "Employee";
+      const result = Employee.role;
+      expect(result).toString("Employee");
     });
-}));
+  }));
